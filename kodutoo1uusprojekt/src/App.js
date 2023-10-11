d48e344da9ce55e424439843ad961964ce13ac69
rkangur <144773420+rkangur@users.noreply.github.com>
2023-10-11 21:29:13 +0300

    sisselogimise kuvamine (ül 8)

--- kodutoo1uusprojekt/src/App.js
+++ kodutoo1uusprojekt/src/App.js
@@ -3,11 +3,47 @@ import {Link, Route, Routes} from 'react-router-dom';
 import Avaleht from './pages/Avaleht';
 import Meist from './pages/Meist';
 import Kontakt from './pages/Kontakt';
+import { useRef, useState } from 'react';
 
 
 function App() {
+
+  const [sisselogitud, muudaSisselogitud] = useState("ei");
+  const [sonum, muudaSonum] = useState("");
+  const kasutajaNimi = useRef(); 
+  const paroolRef = useRef();
+
+  const logiVälja = () => {
+    muudaSisselogitud("ei")
+    muudaSonum("")
+  }
+
+  const logiSisse = () => {
+    if (paroolRef.current.value === "123" && kasutajaNimi.current.value !== ""){
+      muudaSonum (kasutajaNimi.current.value + ", oled sisselogitud!");
+      muudaSisselogitud("jah");
+    } else if (kasutajaNimi.current.value === ""){
+      muudaSonum ("Palun täitke kõik väljad!");
+    } else{
+      muudaSonum ("Vale parool");
+    }
+  }
+
   return (
     <div className="App">
+      <div>{sonum}</div>
+      
+      {sisselogitud === "ei" &&<div>
+        <label>Kasutajanimi:</label> <br />
+        <input ref={kasutajaNimi} type="text"></input> <br />
+        <label>Parool:</label> <br />
+        <input ref={paroolRef} type="password"></input> <br />
+      </div>
+      }
+
+      {sisselogitud === "ei" && <button className="nupp" onClick={logiSisse}>Logi sisse</button>} 
+      {sisselogitud === "jah" && <button className="nupp" onClick={logiVälja}>Logi välja</button>} <br /> <br />
+      
       <Link to="/">
         <button>Avalehele</button>
       </Link>
