5b6b74e1e975b49938a6631c4b6095fb7768ae58
rkangur <144773420+rkangur@users.noreply.github.com>
2023-10-12 09:24:30 +0300

    ternary operator kasutamine (ül 10)
    HTML on kogu aeg peal, css siis kui vajutada. className{variable === true ? "css-klass" : undefined}

--- uudised/src/pages/Kontakt.js
+++ uudised/src/pages/Kontakt.js
@@ -11,9 +11,9 @@ function Kontakt() {
             <span>ajakirjanik Mari: </span>
             <span>mari@setteriuudised.ee</span> 
             {false && <span>+372 5782900</span>}
-            <span onClick={() => muudaN2itaTelSten(!n2itaTelSten)}>ajakirjanik Sten: </span>
+            <span className={n2itaTelSten === true ? "valitud" : undefined} onClick={() => muudaN2itaTelSten(!n2itaTelSten)}>ajakirjanik Sten: </span>
             <span>sten@setteriuudised.ee</span>
-            {n2itaTelSten && <span>+372 5782900</span>}
+            {n2itaTelSten && <span className='valitud'>+372 5782900</span>}
         </div>
         <div className='tere'>info@setteriuudised.ee</div>
         <img alt='koerake' className='kutsu' src='https://animals.net/wp-content/uploads/2019/02/English-Setter-4-650x425.jpg'/>
