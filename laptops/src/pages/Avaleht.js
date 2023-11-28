932eeb971b41e3529b44b85117355d5080d30613
rkangur <144773420+rkangur@users.noreply.github.com>
2023-11-28 23:43:58 +0200

    Ül 26. Laptops täiendus
    LocalStorage'sse massiivina.

--- laptops/src/pages/Avaleht.js
+++ laptops/src/pages/Avaleht.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
 function Avaleht() {
+  const computers = JSON.parse(localStorage.getItem("laptops")) || [];
+
   return (
     <div className='avaleht-text'>
         <div>Tere</div><br></br>
         <div>Siin lehel saad sülearvuteid vaadata ja lisada</div>
+        { computers.map (oneComputer => 
+          <div key={oneComputer.mudel}>
+            <div>{oneComputer.mark}</div>
+            <div>{oneComputer.mudel}</div>
+            <div>{oneComputer.maksumus}</div>
+            <button>Vaata lähemalt</button>
+          </div>)}
     </div>
   )
 }
--- laptops/src/pages/LisaArvuti.js
+++ laptops/src/pages/LisaArvuti.js
@@ -1,26 +1,39 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 function LisaArvuti() {
 
     const [message, setMessage] = useState("Lisa arvuti!");
-    const [n2itaNuppu, uuendaN2itaNuppu] = useState(true);
+    // const [n2itaNuppu, uuendaN2itaNuppu] = useState(true);
+    const markRef = useRef();
+    const mudelRef = useRef();
+    const maksumusRef = useRef();
 
     function addProduct () {
-        setMessage("Arvuti lisatud!");
-        uuendaN2itaNuppu(false);
+      setMessage("Arvuti lisatud!");
+
+      const NewComputer = {
+        "mark": markRef.current.value,
+        "mudel": mudelRef.current.value,
+        "maksumus": maksumusRef.current.value,
+      }
+        const computers = JSON.parse(localStorage.getItem("laptops")) || [];
+        computers.push(NewComputer);
+        localStorage.setItem("laptops", JSON.stringify(computers));
+        
+        // uuendaN2itaNuppu(false);
     }
 
   return (
     <div>
         <div>Sõnum:{message}</div>
         <label>Mark</label><br/>
-        <input type="text"/><br/>
+        <input ref={markRef} type="text"/><br/>
         <label>Mudel</label><br/>
-        <input type="text"/><br/>
+        <input ref={mudelRef} type="text"/><br/>
         <label>Maksumus</label><br/>
-        <input type="number"/><br/>
-        { n2itaNuppu == true && <button onClick={() => addProduct()}>Sisesta</button>}
+        <input ref={maksumusRef} type="number"/><br/>
+        { message === "Lisa arvuti!" && <button onClick={() => addProduct()}>Sisesta</button>}
     </div>
   )
 }
