6c36cfa9ccb898670369d0c62414245e8b62ec72
rkangur <144773420+rkangur@users.noreply.github.com>
2023-12-14 20:54:00 +0200

    lisamine, kustutamine, kui väiksem kui 3, siis tekst punaseks

--- bakery-shop/src/pages/Employees.jsx
+++ bakery-shop/src/pages/Employees.jsx
@@ -1,25 +1,41 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Button, Table } from "react-bootstrap";
 
 function Employees() {
   const [users, setUsers] = useState([]);
+  const url = "https://reqres.in/api/users";
+  const first_nameRef = useRef();
+  const last_nameRef = useRef();
+  const emailRef = useRef();
+  const idRef = useRef();
+  const avatarRef = useRef();
 
   // TODO: Load data from backend service
   useEffect(() => {
-    fetch("https://reqres.in/api/users")
+    fetch(url)
      .then(res => res.json())
-     .then(data => {
-        setUsers(data);
+     .then(userData => {
+        setUsers(userData.data);
      })
   }, []);
 
   const addEmployee = () => {
     // TODO: Add validations
     // TODO: Add an employee to the table
+    users.push( {
+      "id": idRef.current.value,
+      "first_name": first_nameRef.current.value,
+      "last_name": last_nameRef.current.value,
+      "email": emailRef.current.value,
+      "avatar": avatarRef.current.value
+    });
+    setUsers(users.slice());
   }
 
   const deleteEmployee = (index) => {
     // TODO: Delete an employee from the table
+    users.splice(index, 1);
+    setUsers(users.slice());
   }
 
   return (<div>
@@ -32,24 +48,35 @@ function Employees() {
             <th scope="col">Name</th>
             <th scope="col">Email</th>
             {/* <!-- TODO: Add a column for an avatar --> */}
+            <th scope="col">Avatar</th>
             <th scope="col">Actions</th>
           </tr>
-          <tr>
-            <td>Id</td>
-            <td>Name</td>
-            <td>Email</td>
-          </tr>
+          
         </thead>
           <tbody>
-            {Employees.data(users).map((data, index) =>
-            <tr className="input-row" key={index}>
-              <td>{data["id"]}</td>
-              <td>{data["first_name"]} + {data["last_name"]}</td>
-              <td>{data["email"]}</td>
-              <td><Button type="button" variant="danger" onClick={() => deleteEmployee(index)}>Delete</Button></td>
-              <td><Button type="submit" variant="success" onClick={addEmployee}>Add</Button></td>
-          </tr>)}
-        </tbody>
+            {users.map((user, index) =>
+            <tr className="input-row" key={user.id} >
+              <td>{user.id}</td>
+              <td>{user.first_name + " " + user.last_name}</td>
+              <td>{user.email}</td>
+              <td><img src={user.avatar} alt=""/></td>
+              <td>
+                <div><Button type="button" variant="danger" onClick={() => deleteEmployee(index)}>Delete</Button> </div>
+                {/* <div><Button type="submit" variant="success" onClick={addEmployee}>Add</Button></div> */}
+              </td>
+          </tr>)}     
+
+          <tr className="input-row">
+          <td><input type="text" ref={idRef}/></td>
+          <td>
+            <input type="text" ref={first_nameRef} placeholder="First name"/> <br /> <br/>
+            <input type="text" ref={last_nameRef} placeholder="Last name"/>
+          </td>
+          <td><input type="text" ref={emailRef}/></td>
+          <td><input type="text" ref={avatarRef}/></td>
+          <td><Button variant="success" type="submit" onClick={() => addEmployee()} >Add</Button></td>
+        </tr>
+          </tbody>
       </Table>
     </div>
 
--- bakery-shop/src/pages/Products.jsx
+++ bakery-shop/src/pages/Products.jsx
@@ -19,6 +19,8 @@ function Products() {
     { name: 'Chocolate chip cookie', price: 3.25, quantity: 3, store: 'Mustamäe store' }
   ]);
 
+  // products.sort((a, b) => a.price - b.price);
+
   const addProduct = () => {
     const newProduct = {
       name: nameRef.current.value,
@@ -44,18 +46,18 @@ function Products() {
         </thead>
         <tbody>
         {/* TODO: Order the products by price */}
-        {products.map(product => 
+        {products.sort((a, b) => a.price - b.price).map(product => 
             <tr key={product.name + product.price}>
               <td>{product.name}</td>
               <td>{product.price}</td>
               {/*  TODO: Display the quantity in red if it is lower than 3 */}
-              <td>{product.quantity}</td> 
+              {product.quantity < 3 ? <td className="turnRed" >{product.quantity}</td> : <td>{product.quantity}</td>} 
               <td>{product.store}</td>
             </tr>
           )}
         <tr className="input-row">
           <td><input type="text" ref={nameRef} placeholder="Product" className="form-control" /></td>
-          <td><input type="text" ref={priceRef} placeholder="Price" className="form-control" /></td>
+          <td><input type="text" ref={priceRef} placeholder="Price" className="form-control"/></td>
           <td><input type="text" ref={quantityRef} placeholder="Quantity" className="form-control" /></td>
           <td><input type="text" ref={storeRef} placeholder="Store" className="form-control" /></td>
           <td><Button variant="success" type="submit" onClick={() => addProduct()} >Add</Button></td>
